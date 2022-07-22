c6dd62decd3dd3fb8ba924ce3ccf532e9d23f5b1
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-07-22 16:23:28 -0400

    Update SwordScript.cs

--- Assets/Sword Stuff/SwordScript.cs
+++ Assets/Sword Stuff/SwordScript.cs
@@ -14,7 +14,10 @@ public class SwordScript : MonoBehaviour
     float returnpower = 50f;
     float rotateSpeed = 50f;
 
+    bool rotatetome = false;
+
     private Transform targetPos;
+    private Transform playerloc;
 
     void Awake()
     {
@@ -22,28 +25,28 @@ public class SwordScript : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Movement>();
         aim = GameObject.FindGameObjectWithTag("Redicle").GetComponent<AimCode>();
         targetPos = GameObject.FindGameObjectWithTag("Redicle").GetComponent<Transform>();
+        playerloc = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     void Update()
     {
-        Vector2 direction = (Vector2)targetPos.position - swordB.position;
-        
-        float rotate = Vector3.Cross(direction, transform.right).z;
 
-        swordB.angularVelocity = -rotate * rotateSpeed;
+        Vector2 direction = (Vector2)targetPos.position - swordB.position;
+        Vector2 direction2 = (Vector2)playerloc.position - swordB.position;
+        float rotate = Vector3.Cross(direction, transform.right).z;
 
         swordB.angularVelocity = -rotate * rotateSpeed;
 
         if(Input.GetButtonDown("Fire1"))
         {
             swordB.velocity = transform.right * firepower;
-            Debug.Log("I fired my sword");
+            //previous launch code
             //swordB.AddForce(transform.right * 10f, ForceMode2D.Impulse);  # old script that migth get put back but for now it made the sword flail around too much
         }
 
         if(Input.GetButtonDown("Fire2"))
         {
-            
+            swordB.velocity = transform.right * firepower;
         }
 
         if(Input.GetButtonDown("Retrieve"))
