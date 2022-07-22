98e047d64b622d5364409a5e96b81f2691664df6
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-07-22 01:54:01 -0400

    Skeleton patrols

--- Assets/Skeleton.cs
+++ Assets/Skeleton.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Skeleton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 2f;
+    public float distance;
+
+    private bool movingRight = true;
+
+    public Transform grounded;
+
+    void Awake()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(grounded.position, Vector2.down, distance);
+
+        if(groundInfo.collider == false){
+            if(movingRight == true){
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else{
+                transform.eulerAngles = new Vector3(0,0,0);
+                movingRight = true;
+            }
+        }
     }
 }
