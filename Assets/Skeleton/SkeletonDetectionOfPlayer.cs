ea2fab54a1e12d3226c01a9c23c079c17e363f26
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-08-16 10:32:37 -0400

    Removed some code for skeleton. I'll come back to giving him proper player detection at some point

--- Assets/Skeleton/SkeletonDetectionOfPlayer.cs
+++ Assets/Skeleton/SkeletonDetectionOfPlayer.cs
@@ -2,12 +2,3 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CustomEditor (typeof (Skeleton))]
-public class SkeletonDetectionOfPlayer : Editor
-{
-	void OnSceneGUI(){
-		Skeleton fow = (Skeleton)target;
-		Handles.color = Color.white;
-		Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
-	}
-}
