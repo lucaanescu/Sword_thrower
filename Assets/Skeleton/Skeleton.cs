3690418c1cafe7d6cf2fc1e76b724d1950d5a3e7
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-09-19 05:49:39 -0400

    Changed the skeleton ai and added better movement for them

--- Assets/Skeleton/Skeleton.cs
+++ Assets/Skeleton/Skeleton.cs
@@ -6,6 +6,8 @@ public class Skeleton : MonoBehaviour
 {
     public float speed = 2f;
     private float distance = 15f;
+    private Vector2 moveDir;
+    private bool flip;
 
     //view angles for 
     public float viewRadius;
@@ -15,8 +17,14 @@ public class Skeleton : MonoBehaviour
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),Mathf.Cos(angleInDegrees * Mathf.Deg2Rad),0);
     }
 
+    //if player is withing range
+    private bool playerr = false;
+
     //is the ai moving or not
     private bool movingRight = true;
+
+    //player transform
+    private Transform Player;
 
     //checks ground position
     public Transform grounded;
@@ -26,27 +34,40 @@ public class Skeleton : MonoBehaviour
 
     void Awake()
     {
-        
+        Player = GameObject.FindWithTag("Player").transform;
     }
 
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(grounded.position, Vector2.down, distance);
-
-        if(groundInfo.collider == false)
+        if (playerr == false)
         {
-           if(movingRight == true)
-           {
-               transform.eulerAngles = new Vector2(0, -180);
-               movingRight = false;
-           }
-           else
-           {
-               transform.eulerAngles = new Vector2(0,0);
-               movingRight = true;
-           }
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+            RaycastHit2D groundInfo = Physics2D.Raycast(grounded.position, Vector2.down, distance);
+
+            if(groundInfo.collider == false)
+            {
+                if(movingRight == true)
+                {
+                    transform.eulerAngles = new Vector2(0, -180);
+                    movingRight = false;
+                }
+                else
+                {
+                    transform.eulerAngles = new Vector2(0,0);
+                    movingRight = true;
+                }
+            }
+        }
+        else if (playerr == true)
+        {
+            Vector3 chase = (Player.position - transform.position).normalized;
+            float angle = Mathf.Atan2(chase.z, chase.x) * Mathf.Rad2Deg;
+            skelly.rotation = angle;
+            moveDir = chase;
+            
         }
     }
 
---
+++ Assets/Skeleton/SkeletonWalk.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonWalk : MonoBehaviour
+{
+    private float walkSpeed = 200, range;
+
+    public bool mustTurn;
+    public bool mustPatrol;
+    public Rigidbody2D rb;
+    public Transform groundCheckPos, player;
+    public Collider2D bodyCollider;
+
+    public LayerMask groundLayer;
+
+    void Start()
+    {
+        mustPatrol = true;
+    }
+
+    void Update()
+    {
+        if(mustPatrol)
+        {
+            Patrol();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(mustPatrol)
+        {
+            mustTurn = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
+        }
+    }
+
+    void Patrol()
+    {
+        if(mustTurn || bodyCollider.IsTouchingLayers())
+        {
+            Flip();
+        }
+
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
+}
