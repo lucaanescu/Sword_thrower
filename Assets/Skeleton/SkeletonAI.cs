4b8e509f834c9e18d080a1ae02f6dbbad3e16a66
Luca Anescu <48320567+lucaanescu@users.noreply.github.com>
2022-11-21 09:05:47 -0500

    trying a new way to make the ekeletons move and attack. So far its working

--- Assets/Skeleton/SkeletonAI.cs
+++ Assets/Skeleton/SkeletonAI.cs
@@ -6,15 +6,47 @@ public class SkeletonAI : MonoBehaviour
 {
 
     [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCol;
+    [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         cooldownTimer += Time.deltaTime;
 
-        if(cooldownTimer >= attackCooldown){
-            //Attack here
+        if(PlayerInSight())
+        {
+            if(cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("");
+            }
         }
     }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCol.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+        new Vector3(boxCol.bounds.size.x * range, boxCol.bounds.size.y, boxCol.bounds.size.z),
+        0, Vector2.left, 0, playerLayer);
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCol.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+        new Vector3(boxCol.bounds.size.x * range, boxCol.bounds.size.y, boxCol.bounds.size.z));
+    }
+
 }
